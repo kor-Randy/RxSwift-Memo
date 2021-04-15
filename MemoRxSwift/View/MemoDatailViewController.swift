8e537a73ac2f482e30a793c364b91eb76095522e
kor-Randy <randy_95@naver.com>
2021-04-15 17:19:28 +0900

    add viewcontroller class

---
+++ MemoRxSwift/View/MemoDatailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MemoDatailViewController.swift
+//  MemoRxSwift
+//
+//  Created by 지현우 on 2021/04/15.
+//
+
+import UIKit
+
+class MemoDatailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
---
+++ MemoRxSwift/View/MemoListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MemoListViewController.swift
+//  MemoRxSwift
+//
+//  Created by 지현우 on 2021/04/15.
+//
+
+import UIKit
+
+class MemoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
