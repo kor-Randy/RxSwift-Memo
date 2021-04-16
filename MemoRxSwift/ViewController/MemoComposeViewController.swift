0a0666127f1f9df0be049a586e73f7bda4062bb5
kor-Randy <randy_95@naver.com>
2021-04-16 11:25:46 +0900

    Implement ComposeViewController - Binding ViewModel

--- MemoRxSwift/ViewController/MemoComposeViewController.swift
+++ MemoRxSwift/ViewController/MemoComposeViewController.swift
@@ -5,20 +5,54 @@
 //  Created by 지현우 on 2021/04/15.
 //
 
+import Action
+import RxCocoa
+import RxSwift
 import UIKit
 
 class MemoComposeViewController: UIViewController, ViewModelBindableType {
-
-    var viewModel: MemoComposeViewModel!
+    @IBOutlet var cancelButton: UIBarButtonItem!
+    @IBOutlet var saveButton: UIBarButtonItem!
+    @IBOutlet var contentTextView: UITextView!
     
+    let bag = DisposeBag()
+    
+    var viewModel: MemoComposeViewModel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-    func bindViewModel() {
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
+        contentTextView.becomeFirstResponder()
     }
-
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        if contentTextView.isFirstResponder {
+            contentTextView.resignFirstResponder()
+        }
+    }
+    
+    func bindViewModel() {
+        viewModel.title.drive(navigationItem.rx.title)
+            .disposed(by: bag)
+        
+        viewModel.initialText
+            .drive(contentTextView.rx.text)
+            .disposed(by: bag)
+        
+        cancelButton.rx.action = viewModel.cancelAction
+        
+        saveButton.rx.tap
+            .throttle(.milliseconds(500), scheduler: MainScheduler.instance)
+            .withLatestFrom(contentTextView.rx.text.orEmpty)
+            .bind(to: viewModel.saveAction.inputs)
+            .disposed(by: bag)
+    }
 }
